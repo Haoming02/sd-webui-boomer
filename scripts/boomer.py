b2d40280fda07166c76507f35e998268b3e2790f
Haoming <hmstudy02@gmail.com>
2024-08-05 16:27:30 +0800

    lint

--- scripts/boomer.py
+++ scripts/boomer.py
@@ -2,13 +2,14 @@ from modules import script_callbacks, shared
 import modules.scripts as scripts
 import os
 
-STYLE_FIT = os.path.join(scripts.basedir(), 'assets', 'fit.css')
-STYLE_THUMBS = os.path.join(scripts.basedir(), 'assets', 'thumbs.css')
-STYLE_ENLARGE = os.path.join(scripts.basedir(), 'assets', 'enlarge.css')
-STYLE_FOLDER = os.path.join(scripts.basedir(), 'assets', 'folders.css')
+STYLE_FIT = os.path.join(scripts.basedir(), "assets", "fit.css")
+STYLE_THUMBS = os.path.join(scripts.basedir(), "assets", "thumbs.css")
+STYLE_ENLARGE = os.path.join(scripts.basedir(), "assets", "enlarge.css")
+STYLE_FOLDER = os.path.join(scripts.basedir(), "assets", "folders.css")
 
-STYLE = os.path.join(scripts.basedir(), 'style.css')
-section = ('boomer', 'Boomer')
+STYLE = os.path.join(scripts.basedir(), "style.css")
+section = ("boomer", "Boomer")
+
 
 def add_ui_settings():
     shared.opts.add_option("bmr_app_style", shared.OptionInfo(False, 'Duplicate "Apply Style" button back under Generate', section=section))
@@ -23,27 +24,30 @@ def add_ui_settings():
     shared.opts.add_option("bmr_res_extras", shared.OptionInfo(False, "Revert the Extra Networks from the Tree View to the Folder Buttons", section=section))
     shared.opts.add_option("bmr_res_extras_rmb", shared.OptionInfo(False, "|-- Remove the base folder name (eg. Lora) from the folder buttons above", section=section))
 
+
+
 def load_ui_settings():
-    styles = ['\n']
+    styles = []
 
-    if getattr(shared.opts, 'bmr_fit', True):
-        with open(STYLE_FIT, 'r') as FILE:
+    if getattr(shared.opts, "bmr_fit", True):
+        with open(STYLE_FIT, "r") as FILE:
             styles += FILE.readlines()
 
-    if getattr(shared.opts, 'bmr_thumbs', True):
-        with open(STYLE_THUMBS, 'r') as FILE:
+    if getattr(shared.opts, "bmr_thumbs", True):
+        with open(STYLE_THUMBS, "r") as FILE:
             styles += FILE.readlines()
 
-    if getattr(shared.opts, 'bmr_enlarge', False):
-        with open(STYLE_ENLARGE, 'r') as FILE:
+    if getattr(shared.opts, "bmr_enlarge", False):
+        with open(STYLE_ENLARGE, "r") as FILE:
             styles += FILE.readlines()
 
-    if getattr(shared.opts, 'bmr_res_extras', False):
-        with open(STYLE_FOLDER, 'r') as FILE:
+    if getattr(shared.opts, "bmr_res_extras", False):
+        with open(STYLE_FOLDER, "r") as FILE:
             styles += FILE.readlines()
 
-    with open(STYLE, 'w+') as FILE:
+    with open(STYLE, "w+") as FILE:
         FILE.writelines(styles)
+
 
 script_callbacks.on_ui_settings(add_ui_settings)
 script_callbacks.on_before_ui(load_ui_settings)
